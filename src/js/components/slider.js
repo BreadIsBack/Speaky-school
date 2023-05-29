de14b30e140d9bbf4136e40c60a86a712de9be76
BreadIsBack <igleb7117@gmail.com>
2023-05-29 05:21:28 +0300

    добавлен адаптив первой страницы сайта

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -9,3 +9,16 @@ const reviewsSlider = new Swiper('.history__slider-items', {
     clickable: true,
   },
 });
+
+const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+
+bullets.forEach(bullet => {
+  bullet.setAttribute('tabindex', 0);
+  bullet.addEventListener("keyup", function (event) {
+
+    if (event.keyCode === 9) {
+      bullet.click();
+    }
+
+  })
+});
